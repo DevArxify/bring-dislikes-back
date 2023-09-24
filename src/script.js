8f936ce7785b229aceb40ca11170885b8872f0c1
arxify <69035413+DevArxify@users.noreply.github.com>
2023-09-24 14:19:39 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,1 @@
-// the script isn't here, you can get it on https://gist.github.com/ArxifyByte/f2dd40c8827974509390c04fc2fb5eee ;)
+// the script isn't here, you can get it at https://gist.github.com/DevArxify/f2dd40c8827974509390c04fc2fb5eee ;)
